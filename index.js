49074e3f5a4f8e842cd9ff27021a8db7f0ba7911
Adrian Lafond <adrian@disbranded.com>
2015-11-01 14:54:23 -0500

    move index.js back to root, put test.js in data

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-var self = require('sdk/self');
+var data = require("sdk/self").data;
+var pageMod = require("sdk/page-mod");
+
+pageMod.PageMod({
+  include: "*",
+  contentScriptFile: [data.url("test.js")]
+});
 
 // a dummy function, to show how tests work.
 // to see how to test this function, look at test/test-index.js
